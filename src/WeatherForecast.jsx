d874d330de6e014cc0b50f56ce55b7e583706981
Evelyn P <140679177+evelynp111@users.noreply.github.com>
2024-03-27 21:12:26 -0700

    created new component to take care of displaying weather forecast for 1 day, created functions to display max and min temperatures

--- src/WeatherForecast.jsx
+++ src/WeatherForecast.jsx
@@ -1,28 +1,36 @@
-import React from "react";
+import { useState }from "react";
 import "./WeatherForecast.css";
+import WeatherForecastDay from "./WeatherForecastDay";
 import axios from "axios";
 
 export default function WeatherForecast(props){
+    let [loaded, setLoaded] = useState(false);
+    let [forecast, setForecast] = useState(null);
+
 function handleResponse(response){
-    console.log(response.data);
+    console.log(response.data.daily);
+    setForecast(response.data.daily);
+    setLoaded(true);
 }
 
-    let apiKey = "fbf3f590d8fa5cdo2b6a6d68f4tc4ef2";
-    let city = props.city;
-    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=imperial`;
-
-    axios.get(apiUrl).then(handleResponse);
-
-    return (
+    if (loaded) {
+        return (
     <div className="row WeatherForecast">
         <div className="col">
-        <div className="weatherforecast-day">Thu</div>
-            <div className="weatherforecast-icon">🌤️</div>
-            <div className="weatherforecast-temperatures">
-                <span className="weatherforecast-temp-max">64°</span>
-                <span className="weatherforecast-temp-min">55°</span>
-            </div>
+            <WeatherForecastDay data={forecast[0]}/>
         </div>
     </div>
     );
+    } else {
+            let apiKey = "fbf3f590d8fa5cdo2b6a6d68f4tc4ef2";
+            let city = props.city;
+            let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=imperial`;
+            
+            axios.get(apiUrl).then(handleResponse);
+
+        return null;
+    }
+
+
+
 }
---
+++ src/WeatherForecastDay.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+export default function WeatherForecastDay(props) {
+    function maxTemperature(){
+        let temperature = Math.round(props.data.temperature.maximum)
+        return `${temperature}`
+    }
+
+    function minTemperature(){
+        let temperature = Math.round(props.data.temperature.minimum)
+        return `${temperature}`
+    }
+
+    function day () {
+        let date = new Date(props.data.time * 1000);
+        let day = date.getDay();
+        let days = [
+            "Sun",
+            "Mon",
+            "Tue",
+            "Wed", 
+            "Thu",
+            "Fri",
+            "Sat",
+        ]
+        return days[day];
+    }
+
+    return (
+        <div className="WeatherForecastDay">
+            <div className="weatherforecast-day">{day()}</div>
+                <img className="weatherforecast-icon" src={props.data.condition.icon_url} 
+                        alt={props.data.condition.description}></img>
+                <div className="weatherforecast-temperatures">
+                <span className="weatherforecast-temp-max">{maxTemperature()}°</span>
+                <span className="weatherforecast-temp-min">{minTemperature()}°</span>
+            </div>
+        </div>
+    );
+}
